1409b0f8ab246c144b31dbdbec6da6199c1958a9
Puneet Sahai <psahai@automotivemastermind.com>
2021-05-08 19:44:22 +0530

    adding files

--- root-config/src/am-root-config.js
+++ root-config/src/am-root-config.js
@@ -2,7 +2,13 @@ import { registerApplication, start } from "single-spa";
 
 registerApplication({
   name: "ang4",
-  app: () => System.import("//localhost:4202/main.bundle.js"),
+  app: () => {
+    return System.import('//localhost:4202/inline.bundle.js')
+        .then(() => System.import('//localhost:4202/polyfills.bundle.js')
+            .then(() => System.import('//localhost:4202/styles.bundle.js')
+                .then(() => System.import('//localhost:4202/vendor.bundle.js')
+                    .then(() => SystemJS.import('//localhost:4202/main.bundle.js')))));
+  },
   activeWhen: ["/"],
   customProps:  { domElement: document.getElementById('angular-app') }
 });
